a07e8cebec647725808612cbaa8f56b2edb21206
Murat Savran <mrtyy32@yandex.com>
2017-08-01 17:36:31 +0300

    menu falan

--- GarbageCollectorProject/Gcp.Web/Controllers/PersonelController.cs
+++ GarbageCollectorProject/Gcp.Web/Controllers/PersonelController.cs
@@ -11,81 +11,98 @@ using System.Text;
 
 namespace Gcp.Web.Controllers
 {
-    public class PersonelController : Controller
-    {
+	public class PersonelController : Controller
+	{
 		readonly HttpClient _client;
-        string _url = "http://localhost:53723/api/Personel";
-        public PersonelController()
-        {
-            _client = new HttpClient { BaseAddress = new Uri(_url) };
-            _client.DefaultRequestHeaders.Accept.Clear();
-            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        }
+		string _url = "http://localhost:53723/api/Personel";
+		public PersonelController()
+		{
+			_client = new HttpClient { BaseAddress = new Uri(_url) };
+			_client.DefaultRequestHeaders.Accept.Clear();
+			_client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		}
 
 
-        // GET: Personel
-        public ActionResult Index()
-        {
-           return View();
-        }
+		// GET: Personel
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult Create()
-        {
-            return View(new Personel());
-        }
+		public ActionResult Create()
+		{
+			return View(new Personel());
+		}
 
-	    public async Task<ActionResult> Count()
-	    {
+		public async Task<ActionResult> Count()
+		{
 			var responseMessage = await _client.GetAsync(_url);
 			var responseData = responseMessage.Content.ReadAsStringAsync().Result;
 			var personel = JsonConvert.DeserializeObject<List<Personel>>(responseData);
-		    return Json(personel.Count(), JsonRequestBehavior.AllowGet);
-	    }
-        //The Post method
-        [HttpPost]
-        public async Task<ActionResult> Create(Personel p)
-        {
-            var jsonString = JsonConvert.SerializeObject(p);
-            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
-            var responseMessage = await _client.PostAsync(_url,content);
-            return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
-        }
-        //GET Method
-        public async Task<ActionResult> Edit(int id)
-        {
-            var responseMessage = await _client.GetAsync($"{_url}/{id}");
-            if (!responseMessage.IsSuccessStatusCode) return View("Error");
+			return Json(personel.Count(), JsonRequestBehavior.AllowGet);
+		}
+		public async Task<ActionResult> aylikGider()
+		{
+			var responseMessage = await _client.GetAsync(_url);
+			var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+			var personel = JsonConvert.DeserializeObject<List<Personel>>(responseData);
+			var maasToplam = 0;
+			foreach (var item in personel)
+			{
+				if (item.Maas != null)
+				{
+					var pmaas = (decimal)(item.Maas);
+					maasToplam += (int)pmaas;
+				}
+			}
 
-            var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+			return Json((float)maasToplam, JsonRequestBehavior.AllowGet);
+		}
+		//The Post method
+		[HttpPost]
+		public async Task<ActionResult> Create(Personel p)
+		{
+			var jsonString = JsonConvert.SerializeObject(p);
+			var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+			var responseMessage = await _client.PostAsync(_url, content);
+			return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
+		}
+		//GET Method
+		public async Task<ActionResult> Edit(int id)
+		{
+			var responseMessage = await _client.GetAsync($"{_url}/{id}");
+			if (!responseMessage.IsSuccessStatusCode) return View("Error");
 
-            var personel = JsonConvert.DeserializeObject<Personel>(responseData);
+			var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+
+			var personel = JsonConvert.DeserializeObject<Personel>(responseData);
 			return Json(personel, JsonRequestBehavior.AllowGet);
 
-        }
-        //PUT (Update) Method
-        [HttpPost]
-        public async Task<ActionResult> Edit(Personel p)
-        {
+		}
+		//PUT (Update) Method
+		[HttpPost]
+		public async Task<ActionResult> Edit(Personel p)
+		{
 			var jsonString = JsonConvert.SerializeObject(p);
 			var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
 			var responseMessage = await _client.PutAsync($"{_url}/{p.PersonelID}", content);
-	        return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
-        }
-        public async Task<ActionResult> GetPersonelHtml()
-        {
-            var responseMessage = await _client.GetAsync(_url);
+			return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
+		}
+		public async Task<ActionResult> GetPersonelHtml()
+		{
+			var responseMessage = await _client.GetAsync(_url);
 
-            if (!responseMessage.IsSuccessStatusCode) return Json("Error", JsonRequestBehavior.DenyGet);
+			if (!responseMessage.IsSuccessStatusCode) return Json("Error", JsonRequestBehavior.DenyGet);
 
-            var responseData = responseMessage.Content.ReadAsStringAsync().Result;
-            var personel = JsonConvert.DeserializeObject<List<Personel>>(responseData);
-            return View(personel.ToList());
-        }
-        [HttpPost]
-        public async Task<ActionResult> Delete(int id)
-        {
-            var responseMessage = await _client.DeleteAsync(_url + "/" + id);
-            return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
-        }
-    }
+			var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+			var personel = JsonConvert.DeserializeObject<List<Personel>>(responseData);
+			return View(personel.ToList());
+		}
+		[HttpPost]
+		public async Task<ActionResult> Delete(int id)
+		{
+			var responseMessage = await _client.DeleteAsync(_url + "/" + id);
+			return RedirectToAction(responseMessage.IsSuccessStatusCode ? "Index" : "Error");
+		}
+	}
 }
